860ec77eaf64a663e83887964f88ad2fe25918c2
Renato Silvestre <silvestrerenato17@gmail.com>
2023-11-03 19:20:32 -0400

    Finalizing and modifying the index.js file

--- Capitulo_6/exercise_c/index.js
+++ Capitulo_6/exercise_c/index.js
@@ -1,5 +1,4 @@
 const frm = document.querySelector('form')
-const listBtn = document.querySelector('#listBtn')
 const resp = document.querySelector('pre')
 
 const candidates = []
@@ -20,5 +19,20 @@ frm.addEventListener('submit', (ev) => {
 })
 
 listBtn.addEventListener('click', () => {
+  let list = ''
+  candidates.forEach((candidate, i) => list += `${i + 1}. ${candidate.name} - ${candidate.points}\n`)
+  resp.innerText = list
+})
+
+frm.approvedBtn.addEventListener('click', () => {
+  const minScore = Number(prompt('Qual a pontuação minima para a proxima etapa?'))
+  const approvedCandidates = candidates.filter(can => can.points >= minScore)
+  const orderedCandidates = approvedCandidates.sort((a, b) => b.points - a.points)
   
+  let list = ''
+  orderedCandidates.forEach((can, i) => {
+    list += `${i + 1}. ${can.name} - ${can.points}\n`
+  })
+  
+  resp.innerText = list
 })
